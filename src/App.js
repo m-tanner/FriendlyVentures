b0dd55647630189074d4e7a41c316d9fa573af0a
m-tanner <tanner.mbt@gmail.com>
2020-06-28 17:02:19 -0700

    initial commits, slight mods for k8s to function properly

--- src/App.js
+++ src/App.js
@@ -1,77 +1,55 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactGA from 'react-ga';
+import $ from 'jquery';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
 
-function App() {
-  return (
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-          <hr />
+class App extends Component {
 
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/health">
-              <Health />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-  );
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: 'bar',
+            resumeData: {}
+        };
 
-function Home() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
-}
+        ReactGA.initialize('UA-110570651-1');
+        ReactGA.pageview(window.location.pathname);
 
-function Health() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    }
+
+    getResumeData() {
+        $.ajax({
+            url: '/resumeData.json',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({resumeData: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.getResumeData();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header data={this.state.resumeData.main}/>
+                <About data={this.state.resumeData.main}/>
+                <Resume data={this.state.resumeData.resume}/>
+                <Footer data={this.state.resumeData.main}/>
+            </div>
+        );
+    }
 }
 
 export default App;
